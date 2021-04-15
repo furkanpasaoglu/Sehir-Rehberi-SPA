70a810118f8c51455ec2275e3aed9e9c865270a7
Furkan Paşaoğlu <furkan.pasaoglu@hotmail.com>
2021-04-15 18:17:28 +0300

    Updated

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Router } from "@angular/router";
+import { appRoutes } from "./routes";
+import { NgxGalleryModule } from "ngx-gallery";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {NgxEditorModule} from 'ngx-editor'
+import {FileUploadModule} from 'ng2-file-upload'
+
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { CityComponent } from "./city/city.component";
+import { CityDetailComponent } from "./city/city-detail/city-detail.component";
+import {CityAddComponent} from "./city/city-add/city-add.component"
+import {AlertifyService} from './services/alertify.service'
+import { RegisterComponent } from './register/register.component';
+import { PhotoComponent } from './photo/photo.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent,
+      CityComponent,
+      CityDetailComponent,
+      CityAddComponent,
+      RegisterComponent,
+      PhotoComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
+      FormsModule, ReactiveFormsModule,
+      NgxEditorModule,
+      FileUploadModule
+   ],
+  providers: [AlertifyService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/routes.ts
@@ -0,0 +1,13 @@
+import { CityComponent } from "./city/city.component";
+import {Routes} from "@angular/router"
+import { CityDetailComponent } from "./city/city-detail/city-detail.component";
+import { CityAddComponent } from "./city/city-add/city-add.component";
+import { RegisterComponent } from "./register/register.component";
+
+export const appRoutes : Routes = [
+  { path: "city", component: CityComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "cityadd", component: CityAddComponent },
+  { path: "cityDetail/:cityId", component: CityDetailComponent },
+  { path: "**", redirectTo: "city", pathMatch: "full" }
+];
